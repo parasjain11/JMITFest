a96a4e5eca8ef7ea82f4bf86ac7cb6d709863f83
Damandeep Singh <damandeep0034@gmail.com>
2016-10-11 16:40:54 +0530

    fragment_add_event

--- app/src/main/java/com/jmit/festmanagement/fragments/AddEvent.java
+++ app/src/main/java/com/jmit/festmanagement/fragments/AddEvent.java
@@ -1,10 +1,12 @@
 package com.jmit.festmanagement.fragments;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
@@ -18,7 +20,7 @@ import java.util.Calendar;
 
 public class AddEvent extends BaseFragment {
 
-    EditText etStartTime;
+    EditText etStartTime,etEndTime,etStartDate,etEndDate;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +28,11 @@ public class AddEvent extends BaseFragment {
         View rootView = inflater.inflate(R.layout.fragment_add_event, container, false);
 
         etStartTime=(EditText)rootView.findViewById(R.id.tfStartTime);
+        etEndTime=(EditText)rootView.findViewById(R.id.tfEndTime);
+        etStartDate=(EditText) rootView.findViewById(R.id.tfStartDate);
+        etEndDate=(EditText) rootView.findViewById(R.id.tfEndDate);
+
+
         etStartTime.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -44,6 +51,72 @@ public class AddEvent extends BaseFragment {
                 mTimePicker.setTitle("Select Time");
                 mTimePicker.show();
 
+            }
+        });
+        etEndTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        etEndTime.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+
+        etStartDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dpd = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                etStartDate.setText(dayOfMonth + "-"
+                                        + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.show();
+            }
+        });
+
+        etEndDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dpd = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                etEndDate.setText(dayOfMonth + "-"
+                                        + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.show();
             }
         });
 
