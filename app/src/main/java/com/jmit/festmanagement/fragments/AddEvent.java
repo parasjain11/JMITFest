b7b5cc8b3e4acb0a5178899b7c30f753c75f5adc
Damandeep Singh <damandeep0034@gmail.com>
2016-10-11 14:44:42 +0530

    View Registrations Fragment Layout and Java file Changes in ViewRegistrations fragment_view_registrations

--- app/src/main/java/com/jmit/festmanagement/fragments/AddEvent.java
+++ app/src/main/java/com/jmit/festmanagement/fragments/AddEvent.java
@@ -1,9 +1,58 @@
 package com.jmit.festmanagement.fragments;
 
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import com.jmit.festmanagement.R;
+
+import java.util.Calendar;
+
 /**
  * Created by arpitkh96 on 11/10/16.
  */
 
 public class AddEvent extends BaseFragment {
+
+    EditText etStartTime;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_add_event, container, false);
+
+        etStartTime=(EditText)rootView.findViewById(R.id.tfStartTime);
+        etStartTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        etStartTime.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+
+        return rootView;
+    }
+
+    public void timePopUp(View view)
+    {
+        //showDialog(TIME_DIALOG_ID);
+    }
 
 }
